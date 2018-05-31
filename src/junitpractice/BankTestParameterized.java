0ae73d9b45d6cacfb3aa08e0f87210659f391f1b
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-05-30 22:25:08 -0400

    implemented parameterized tests for deposit() with five different values

--- src/junitpractice/BankTestParameterized.java
+++ src/junitpractice/BankTestParameterized.java
@@ -6,19 +6,30 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(Parameterized.class)
 public class BankTestParameterized {
 
     private BankAccount myAccount;
+    private double amount;
+    private boolean branch;
+    private double expected;
 
-    @org.junit.Before;
+    public BankTestParameterized(double amount, boolean branch, double expected) {
+        this.amount = amount;
+        this.branch = branch;
+        this.expected = expected;
+    }
+
+    @org.junit.Before
     public void setup(){
         myAccount = new BankAccount("Ranjith", "Kumar", 1000.00, BankAccount.CHECKING);
         System.out.println("Inside setup(): Running Tests.... ");
     }
 
     @Parameterized.Parameters
-    public static Collection<Object[]> testConditions(){
+    public static Collection<Object[]> testCondition(){
         return Arrays.asList(new Object[][]{
                 {100.00, true, 1100.00},
                 {200.00, true, 1200.00},
@@ -26,5 +37,11 @@ public class BankTestParameterized {
                 {2003.00, true, 3003.00},
                 {530.00, true, 1530.00}
         });
+    }
+
+    @org.junit.Test
+    public void parameterized_deposit() {
+        myAccount.deposit(amount,branch);
+        assertEquals(expected, myAccount.getBalance(), 0);
     }
 }
