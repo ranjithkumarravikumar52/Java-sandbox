1c8fb227091e400ef75f5e3fc5f37571308baa4f
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-05-31 11:50:46 -0400

    removed extra spaces in all the methods of parameterized class

--- src/JUnitChallenge1/RemovePairsTestParameterized.java
+++ src/JUnitChallenge1/RemovePairsTestParameterized.java
@@ -1,6 +1,5 @@
 package JUnitChallenge1;
 
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -23,28 +22,24 @@ public class RemovePairsTestParameterized {
         this.expected = expected;
     }
 
-    @org.junit.Before
-    public void setup(){
-        util = new Utilities();
-    }
-
     @Parameterized.Parameters
     public static Collection<Object[]> testCondition() {
-        return Arrays.asList(new Object[][] {
-                                 {"ABCDEFF", "ABCDEF"},
-                                 {"AB88EFFG", "AB8EFG"},
-                                 {"112233445566", "123456"},
-                                 {"ZYZQQB", "ZYZQB"},
-                                 {"A", "A"}
+        return Arrays.asList(new Object[][]{
+                {"ABCDEFF", "ABCDEF"},
+                {"AB88EFFG", "AB8EFG"},
+                {"112233445566", "123456"},
+                {"ZYZQQB", "ZYZQB"},
+                {"A", "A"}
         });
+    }
 
-
+    @org.junit.Before
+    public void setup() {
+        util = new Utilities();
     }
 
     @org.junit.Test
     public void removePairs_Parameterized() {
         assertEquals(expected, util.removePairs(input));
     }
-
-
 }
