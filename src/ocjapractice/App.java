d77bb93724479478045689076bf2be38e80085c5
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-06-14 02:23:35 -0400

    added app.java to practice OCJA snippets

---
+++ src/ocjapractice/App.java
@@ -0,0 +1,10 @@
+package ocjapractice;
+
+public class App {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = new String("Hello");
+
+        System.out.println(s1.equals(s2));
+    }
+}
