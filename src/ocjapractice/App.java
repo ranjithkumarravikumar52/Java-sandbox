cc13c01167d93de7092db9cd60f5c545ab5a81cc
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-06-14 20:41:12 -0400

    updated notes about strings

--- src/ocjapractice/App.java
+++ src/ocjapractice/App.java
@@ -2,9 +2,8 @@ package ocjapractice;
 
 public class App {
     public static void main(String[] args) {
-        String s1 = "Hello";
-        String s2 = new String("Hello");
-
-        System.out.println(s1.equals(s2));
+        String hi = "hi";
+        String anotherHi = "hi";
+        System.out.println(hi==anotherHi);
     }
 }
