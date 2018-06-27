d09891b1ae264c8de8e2f20b460ed03a13480c83
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-06-26 20:14:00 -0400

    swap

--- src/collections/main/App.java
+++ src/collections/main/App.java
@@ -6,24 +6,28 @@ public class App {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("AMC Charlotte", 4, 3);
 
-        System.out.println("Shuffle");
-        Collections.shuffle(theatre.getSeats());
-        theatre.displaySeats();
+//        System.out.println("Shuffle");
+//        Collections.shuffle(theatre.getSeats());
+//        theatre.displaySeats();
+//
+//        System.out.println();
+//
+//        System.out.println("Sorted");
+//        Collections.sort(theatre.getSeats());
+//        theatre.displaySeats();
+//
+//        System.out.println();
+//
+//        System.out.println("Reverse");
+//        Collections.reverse(theatre.getSeats());
+//        theatre.displaySeats();
+//
+//        System.out.println("Min value(by seatname) " +Collections.min(theatre.getSeats()).getSeatNumber());
+//        System.out.println("Max value(by seatname) " +Collections.max(theatre.getSeats()).getSeatNumber());
 
         System.out.println();
-
-        System.out.println("Sorted");
-        Collections.sort(theatre.getSeats());
+        Collections.swap(theatre.getSeats(), 0, theatre.getSeats().size() - 1);
         theatre.displaySeats();
-
-        System.out.println();
-
-        System.out.println("Reverse");
-        Collections.reverse(theatre.getSeats());
-        theatre.displaySeats();
-
-        System.out.println("Min value(by seatname) " +Collections.min(theatre.getSeats()).getSeatNumber());
-        System.out.println("Max value(by seatname) " +Collections.max(theatre.getSeats()).getSeatNumber());
 
     }
 }
