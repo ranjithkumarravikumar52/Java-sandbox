5ad0f3df55509850a25636ad321bd0569d4da3fa
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-06-28 19:08:23 -0400

    end of local class example

--- src/innerclasses/main/App.java
+++ src/innerclasses/main/App.java
@@ -1,28 +1,43 @@
 package innerclasses.main;
 
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button button = new Button("Print");
+
     public static void main(String[] args) {
-        Gearbox mcLaren = new Gearbox(6);
 
-//        //how to create gear class
-//        Gearbox.Gear firstGear = mcLaren.new Gear(1, 12.3);
-//        System.out.println(firstGear.driveSpeed(1000));
-//
-////        Gearbox.Gear secondGear = new Gearbox.Gear(.1, 12.3); //not an enclosing class error
-        mcLaren.operateClutch(true);
-        mcLaren.changeGear(1);
-        System.out.println(mcLaren.wheelSpeed(1000));
-        mcLaren.operateClutch(false);
+        class ClickListener implements Button.OnClickListener {
 
-        mcLaren.changeGear(2);
-        System.out.println(mcLaren.wheelSpeed(3000));
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
 
-        mcLaren.operateClutch(true);
-        mcLaren.changeGear(3);
-        mcLaren.operateClutch(false);
-        System.out.println(mcLaren.wheelSpeed(6000));
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked!");
+            }
+        }
 
+        button.setOnClickListener(new ClickListener());
+        listen();
 
+    }
 
+    private static void listen() {
+        boolean quit = false;
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    button.onClick();
+            }
+        }
     }
 }
