bfcd2ab462f4c5cfda7b87ebd26c9aba345dfe41
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-06-28 13:51:59 -0400

    removed all references to inner class in the main

--- src/innerclasses/main/App.java
+++ src/innerclasses/main/App.java
@@ -4,11 +4,27 @@ public class App {
     public static void main(String[] args) {
         Gearbox mcLaren = new Gearbox(6);
 
-        //how to create gear class
-        Gearbox.Gear firstGear = mcLaren.new Gear(1, 12.3);
-        System.out.println(firstGear.driveSpeed(1000));
+//        //how to create gear class
+//        Gearbox.Gear firstGear = mcLaren.new Gear(1, 12.3);
+//        System.out.println(firstGear.driveSpeed(1000));
+//
+////        Gearbox.Gear secondGear = new Gearbox.Gear(.1, 12.3); //not an enclosing class error
+        mcLaren.addGear(1, 5.3);
+        mcLaren.addGear(2, 10.6);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.operateClutch(false);
 
-//        Gearbox.Gear secondGear = new Gearbox.Gear(.1, 12.3); //not an enclosing class error
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+
 
 
     }
--- src/innerclasses/main/Gearbox.java
+++ src/innerclasses/main/Gearbox.java
@@ -6,6 +6,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
     private int currentGear = 0;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -14,7 +15,35 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear{
+    public void operateClutch(boolean in){
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio){
+        if(number > 0 && number <= maxGears){
+            gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if(newGear >= 0 && newGear < this.gears.size() && clutchIsIn){
+            this.currentGear = newGear;
+            System.out.println("newGear = " + newGear);
+        }else{
+            System.out.println("GRIND!");
+        }
+    }
+
+    public double wheelSpeed(int revs){
+        if(clutchIsIn){
+            System.out.println("Scream !!!");
+            return 0.0;
+        }else{
+            return revs * gears.get(currentGear).getRatio();
+        }
+    }
+
+    private class Gear{
         private int gearNumber;
         private double ratio;
 
@@ -25,6 +54,10 @@ public class Gearbox {
 
         public double driveSpeed(int revs){
             return revs * (this.ratio);
+        }
+
+        public double getRatio() {
+            return ratio;
         }
     }
 }
