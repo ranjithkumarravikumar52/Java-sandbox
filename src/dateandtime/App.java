803598f384e608b35c0849136eee67f1a6c47cfe
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-07-01 21:13:23 -0400

    ISO format type

--- src/dateandtime/App.java
+++ src/dateandtime/App.java
@@ -1,18 +1,30 @@
 package dateandtime;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class App {
 
     public static void main(String[] args) {
+//        periods();
+        //format date and time
+        LocalDate date = LocalDate.of(2011, Month.AUGUST, 4);
+        LocalTime localTime = LocalTime.of(14, 56, 32);
+        LocalDateTime localDateTime = LocalDateTime.of(date, localTime);
+
+        //format type
+        System.out.println(localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        System.out.println(localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println(localDateTime.format(DateTimeFormatter.ISO_LOCAL_TIME));
+    }
+
+    private static void periods() {
         LocalDate start = LocalDate.of(2017, Month.JANUARY, 1);
         LocalDate end = LocalDate.of(2017, Month.APRIL, 3);
 
 //        System.out.println(start.toEpochDay()+" days");
 
-        cleanAnimalCage(start,end);
+        cleanAnimalCage(start, end);
     }
 
     /**
