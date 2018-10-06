c95df9b4b348c59b16387075383bfed121a5b796
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-10-05 22:04:52 -0400

    factory pattern using enum types

--- src/designpatterns/factorypattern/vehiclefactory/VehicleFactory.java
+++ src/designpatterns/factorypattern/vehiclefactory/VehicleFactory.java
@@ -3,17 +3,18 @@ package designpatterns.factorypattern.vehiclefactory;
 import designpatterns.factorypattern.vehicles.*;
 
 public class VehicleFactory {
-    public Vehicle getVehicle(String vehicleType){
-        if(vehicleType.equalsIgnoreCase("TRUCK")){
+
+    public Vehicle getVehicle(VehicleType vehicleType) {
+       /* if (vehicleType.equalsIgnoreCase("TRUCK")) {
             return new Truck();
-        }else if(vehicleType.equalsIgnoreCase("BUS")){
+        } else if (vehicleType.equalsIgnoreCase("BUS")) {
             return new Bus();
-        }else if(vehicleType.equalsIgnoreCase("CAR")){
+        } else if (vehicleType.equalsIgnoreCase("CAR")) {
             return new Car();
-        }else if(vehicleType.equalsIgnoreCase("ELECTRIC")){
+        } else if (vehicleType.equalsIgnoreCase("ELECTRIC")) {
             return new ElectricCar();
-        }
-        else
-            return null;
+        } else
+            return null*/;
+            return vehicleType.getVehicle();
     }
 }
